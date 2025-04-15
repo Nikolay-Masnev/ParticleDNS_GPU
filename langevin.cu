f672e1963d622b6cb7a72ae51303efbdaed892dc
Nikolay Masnev <masnev.ng@phystech.edu>
2025-04-15 15:38:02 +0300

    commit

--- langevin.cu
+++ langevin.cu
@@ -30,42 +30,31 @@ __device__ double Sigma(double r)
 
 __device__ double D(double r, double L)
 {
-    return (1 + pow(r/L, 2));
+    //return (1 + pow(r/L, 2));
+    return 0;
 }
 
 __device__ double dD_dx(double x, double y, double L)
 {
-    return 2 * x/ (L * L);
+    //return 2 * x/ (L * L);
+    return 0;
 }
 
 __device__ double dD_dy(double x, double y, double L)
 {
-    return 2 * y/ (L * L);
+    //return 2 * y/ (L * L);
+    return 0;
 }
 
 __device__ double K(double r, double L)
 {
-    return (1 + pow(r/L, 2));
-}
-
-__device__ double dK_dx(double x, double y, double L)
-{
-    return 2 * x/ (L * L);
-}
-
-__device__ double dK_dy(double x, double y, double L)
-{
-    return 2 * y/ (L * L);
-}
-
-__device__ double M(double r, double L)
-{
-    return 1e2 * (1 + 1e4 * pow(r/L, 2) );
+    return  100 * (1 + pow(r/L, 2));
 }
 
 __device__ double tau_corr(double r, double L)
 {
-    return 1/sqrt(15 * M(r, L));
+    //return 2 * 1e-2/(1 + 10 * pow(r/L, 2));
+    return 1000;
 }
 
 __global__ void numericalProcedure(unsigned long long int *d_concentration,
@@ -80,14 +69,14 @@ __global__ void numericalProcedure(unsigned long long int *d_concentration,
     double a = params.a;
     double r_bin = L / size;
     double sqrt12 = sqrt((float)12);
-    double tau_invert = pow(L, 2) / (pow(a,2) * Re) * 100;
+    double tau_invert = pow(L, 2) / (pow(a,2) * Re);
     double tau = 1/tau_invert;
     double dt = tau/10;
     double sqrt_dt = sqrt(dt);
     double dt_tau_invert = dt * tau_invert;
 
     double dx = 0, dy = 0, x = 0, y = 0;
-    double dw_x = 0, dw_y = 0, w_x = 0, w_y = 0;
+    double dw_x = 0, dw_y = 0, w_x = 0, w_y = 0, w_x_old = 0, w_y_old = 0;
 
     unsigned long long int steps = params.numSteps;
     unsigned long long int ind = 0;
@@ -103,33 +92,34 @@ __global__ void numericalProcedure(unsigned long long int *d_concentration,
     double dW3 = 0;
     double dW4 = 0;
 
+    double rho = 0;
+    double sqrt_one_rho = 0;
+
     __syncthreads();
 
     for(unsigned long long int i = 0; i < steps; ++i)
     {   
-        dW1 = curand_normal(&localState);
-        dW2 = curand_normal(&localState);
-        dW3 = curand_normal(&localState);
-        dW4 = curand_normal(&localState);
+	rho = exp(-dt/tau_corr(r, L));
+        sqrt_one_rho = sqrt(1 - rho * rho);
+	
+	//rho = 0;
+	//sqrt_one_rho = 1;
 
-        dx = (w_x + dD_dx(x,y,L)) * dt + sqrt(2 * D(r, L)) * sqrt_dt * dW1;
-        dy = (w_y + dD_dy(x,y,L)) * dt + sqrt(2 * D(r, L)) * sqrt_dt * dW2;
+        dW1 = sqrt_one_rho * curand_normal(&localState) + rho * dW1;
+        dW2 = sqrt_one_rho * curand_normal(&localState) + rho * dW2;
+        dW3 = sqrt_one_rho * curand_normal(&localState) + rho * dW3;
+        dW4 = sqrt_one_rho * curand_normal(&localState) + rho * dW4;
 
-        dw_x = (-tau_invert * w_x + dK_dx(x, y, L)) * dt + sqrt(2 * K(r, L)) * sqrt_dt * dW3;
-        dw_y = (-tau_invert * w_y + dK_dy(x, y, L)) * dt + sqrt(2 * K(r, L)) * sqrt_dt * dW4;
+        dx = (w_x + 0 * dD_dx(x,y,L)) * dt + 0 * sqrt(2 * D(r, L)) * sqrt_dt * dW1;
+        dy = (w_y + 0 * dD_dy(x,y,L)) * dt + 0 * sqrt(2 * D(r, L)) * sqrt_dt * dW2;
+
+        dw_x = (-tau_invert * w_x) * dt + sqrt(2 * K(r, L)) * sqrt_dt * dW3;
+        dw_y = (-tau_invert * w_y) * dt + sqrt(2 * K(r, L)) * sqrt_dt * dW4;
 
         x += dx;
         y += dy;
-
-        if(w_x * (w_x + dw_x) < 0)
-            w_x = 0;
-        else
-            w_x += dw_x;
-
-        if(w_y * (w_y + dw_y) < 0)
-            w_y = 0;
-        else
-            w_y += dw_y;
+        w_x += dw_x;
+        w_y += dw_y;
 
         r = sqrt(x*x + y*y);
 
@@ -137,12 +127,12 @@ __global__ void numericalProcedure(unsigned long long int *d_concentration,
         {
             x -= dx;
             y -= dy;
-
-            if(w_x * x + w_y * y > 0)
-            {
-                w_x = (x/r) * w_x + (y/r) * w_y;
-                w_y = -(y/r) * w_x + (x/r) * w_y;
-            }
+	    w_x = -w_x;
+	    w_y = -w_y;
+	    dW1 = -dW1;
+	    dW2 = -dW2;
+	    dW3 = -dW3;
+	    dW4 = -dW4;
         }
 
         r = sqrt(x*x + y*y);
@@ -151,7 +141,7 @@ __global__ void numericalProcedure(unsigned long long int *d_concentration,
         ind = min(int(r / r_bin), int(size-1));
         atomicAdd(&d_concentration[ind], 1);
 
-	    if(int(r/r_bin) > size)
+	if(int(r/r_bin) > size)
         {
             printf("r = %f\n", r);
         }
